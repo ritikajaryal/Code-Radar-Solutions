75ea22f52760829bbe251784a1b2d5c1447f589a
ritikajaryal <ritika1498.becse24@chitkara.edu.in>
2025-02-05 14:11:30 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    char op;
+    int a,b;
+    scanf("%d %d %c",&a,&b,&op);
+    if (op=='+'){
+        printf("%d",a+b);
+    }
+    else if (op=="-"){
+        printf("%d",a-b);
+    }
+    else if (op=="*"){
+        printf("%d",a*b);
+    }
+    else if(op=="/"){
+        printf("%d",a/b);
+    }
+    else{
+        printf("Invslid opeation")
+    }
+    return 0;
+}
