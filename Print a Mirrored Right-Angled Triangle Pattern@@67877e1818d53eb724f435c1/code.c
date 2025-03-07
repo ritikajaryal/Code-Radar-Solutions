6836bd0446b7de4aff9bc01083d6770a31811608
Ritika Jaryal <ritika1498.becse24@chitkara.edu.in>
2025-03-07 17:02:08 +0530

    Solution to Print a Mirrored Right-Angled Triangle Pattern

--- Print a Mirrored Right-Angled Triangle Pattern@@67877e1818d53eb724f435c1/code.c
+++ Print a Mirrored Right-Angled Triangle Pattern@@67877e1818d53eb724f435c1/code.c
@@ -1,25 +1,16 @@
-// Your code here...
 #include <stdio.h>
-
-int main() {
-    int n, i, j, space;
-
-    // Get the number of rows from the user
-    printf("Enter the number of rows: ");
-    scanf("%d", &n);
-
-    // Loop for rows
-    for (i = 1; i <= n; i++) {
-        // Print spaces
-        for (space = 1; space <= n - i; space++) {
+int main(){
+    int i,j,n,space;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(space=1;space<=n-i;space++){
             printf(" ");
         }
-        // Print stars
-        for (j = 1; j <= i; j++) {
+        for(j=1,j<=i;j++){
             printf("*");
         }
         printf("\n");
     }
-
     return 0;
+
 }
