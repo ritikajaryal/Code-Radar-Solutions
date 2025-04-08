838efdd4f65581da9b4d8cc289d40a7ae0a364ac
Ritika Jaryal <ritika1498.becse24@chitkara.edu.in>
2025-04-08 18:12:47 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,19 +1,34 @@
-// Your code here...
 #include <stdio.h>
-int main(){
-    int a,x,totalpairs=0;
-    scanf("%d %d",&a,&x);
-    int arr[a];
-    for(int i=0;i<a;i++){
-        for(int j=i+1;j<a;j++){
-            if(arr[i]+arr[j]==x){
-                totalpairs++;
-                
-            }
-           // printf("%d %d",arr[i],arr[j]);
+#include <stdlib.h>
+
+#define MAX 1000
+
+// Hashing approach for positive and negative values
+int hash[MAX * 2] = {0};  // To store frequency of elements
+
+void findPairs(int arr[], int n, int target) {
+    //printf("Unique pairs with sum %d:\n", target);
+
+    for (int i = 0; i < n; i++) {
+        int complement = target - arr[i];
+
+        // Check if complement exists in hash table
+        if (hash[complement + MAX]) {
+            printf("(%d, %d)\n", arr[i], complement);
+            hash[complement + MAX] = 0;  // Mark as used
+            hash[arr[i] + MAX] = 0;      // Mark current as used
+        } else {
+            hash[arr[i] + MAX] = 1;
         }
-        //printf("%d %d",arr[i],arr[j]);
     }
-    printf("%d %d",arr[i],arr[j]);
+}
+
+int main() {
+    int arr[] = {1, 5, 7, -1, 5, 2, -1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 6;
+
+    findPairs(arr, n, target);
+
     return 0;
 }
